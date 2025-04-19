30a3b6a0f0415520ce4d77e06a2055a360cd5ed1
R.J Husnain <rjmaster261@gmail.com>
2025-04-19 21:28:34 +0500

    Creating UI for Forms

--- partials/_signup.php
+++ partials/_signup.php
@@ -3,21 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup on R.J Tech</title>
-    <link rel="stylesheet" href="src/style.css">
+    <title>Signup to R.J Tech</title>
+    <link rel="stylesheet" href="form.css">
 </head>
-<body>
-    <!-- Forms -->
-    <div class="signupForm flex">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-            <label for="cPassword">Confirm Password:</label>
-            <input type="cPassword" name="cPassword" id="cPassword">
-            <button type="submit">Login</button>
-        </form>
+<body class="flex">
+    <!-- login -->
+    <div class="loginForm flex">
+        <div class="form">
+            <h2>Signup to <span>R.J Tech</span></h2>
+            <p>Already have an Account <a href="/forum/partials/_login.php">Login</a></p>
+            <form class="flex" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+<div class="flex">
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" required>
+</div>
+
+<div class="flex">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" required>
+</div>
+
+<div class="flex">
+    <label for="cpassword">Confirm Password</label>
+    <input type="cpassword" id="cpassword" name="cpassword" required>
+</div>
+    <button type="submit">Login</button>
+            </form>
+        </div>
+        <div class="img">
+            <img src="../images/form.jpg" alt="Login Illustration">
+        </div>
     </div>
 </body>
 </html>
