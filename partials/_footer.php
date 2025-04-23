ecbc728eebc2558966f88b9c18f5908905c3ed2b
R.J Husnain <rjmaster261@gmail.com>
2025-04-23 09:54:19 +0500

    links

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,12 +2,24 @@
 echo'
 <div class="footer flex">
         <div class="icons flex">
-            <img src="images/fb.png" alt="">
+        <a href="https://www.facebook.com/profile.php?id=61552545887696"> 
+        <img src="images/fb.png" alt="">
+        </a>
+         <a href="https://www.fiverr.com/s/Ay0m5Qa" target="_blank"> 
             <img src="images/fi.png" alt="">
+             </a>
+             <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDZdDcmDQVvTtMzbwxqbgLTTDgkvwBqzBWVxvtmsllrrSGqCCJQPqRvZMVZGMdXZgBQvQzg" target="_blank"> 
             <img src="images/gm.png" alt="">
+             </a>
+             <a href="https://www.instagram.com/rjhusnain90/" target="_blank"> 
             <img src="images/in.png" alt="">
+             </a>
+             <a href="https://www.linkedin.com/in/r-j-husnain-3a1494303/" target="_blank"> 
             <img src="images/li.png" alt="">
+             </a>
+             <a href="https://www.youtube.com/channel/UCPOhPpYYsLMw13rVtqBGjHA" target="_blank"> 
             <img src="images/yt.png" alt="">
+             </a>
         </div>
         <div class="footerText">
             © 2025 All Rights Reserved | R.J Tech
